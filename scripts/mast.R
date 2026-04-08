3307664d3ecfb891dddddd3cbf4d34d1ba220814
JShermanK1 <JShermanK1@gmail.com>
2026-04-08 16:28:26 -0400

    - PCA regression using DESeq2 - update mast script to fix nested model

--- scripts/mast.R
+++ scripts/mast.R
@@ -6,21 +6,40 @@ library(parallelly)
 options(mc.cores = availableCores())
 
 sce <- read_h5ad(
-  "data/combicells.h5ad",
+  "data/merged_w_others_tumor.h5ad",
   as = "SingleCellExperiment"
 )
 
 cd2 <- colSums(assay(sce, i = 4) > 0)
 colData(sce)$detected <- scale(cd2)
 
+colData(sca)$idents <- as.factor(
+    gsub(
+        " ",
+        "_",
+        as.character(colData(sca)$idents),
+    )
+)
+
+colData(sca)$disease_timing <- as.factor(
+    gsub(
+        " ",
+        "_",
+        as.character(colData(sca)$disease_timing)
+    )
+)
+
 sca <- SceToSingleCellAssay(sce)
 zlm_mod <- zlm(
-  ~ idents + diagnosis + disease_timing,
+  ~ idents + (1 | disease_timing / sample_id) + detected,
   sca,
   exprs_values = "log",
+  method = "glmer",
+  ebayes = F,
+  strictConvergence = F,
 )
 
 saveRDS(
   zlm_mod,
-  file = "data/mast_out_combi.rds"
+  file = "data/mast_out_tumor.rds"
 )
